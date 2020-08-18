c9bc13693b66123a8e3d983507237fd7e67aa007
Okiemute Omuta <iamkheme@gmail.com>
2020-08-18 19:34:09 +0100

    Create testExternalBookSearchWithoutResults
    Add new test to ensure no results search is in correct format

---
+++ app/Http/Controllers/ExternalBookController copy.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Controller for external book search on the fire & ice API
+ *
+ * PHP version 7
+ *
+ * @category  Controller
+ * @package   App\Http\Controllers
+ * @author    Okiemute Omuta <omuta.okiemute@gmail.com>
+ * @copyright 2020 Okiemute Omuta. All rights reserved.
+ * @license   All rights retained
+ * @link      https://twitter.com/kheme
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+/**
+ * Main ExternalBookController class
+ *
+ * @category  Controller
+ * @package   App\Http\Controllers
+ * @author    Okiemute Omuta <omuta.okiemute@gmail.com>
+ * @copyright 2020 Okiemute Omuta. All rights reserved.
+ * @license   All rights retained
+ * @link      https://twitter.com/kheme
+ */
+class ExternalBookController extends Controller
+{
+    /**
+     * List books from the fire and ice database that match the given search criteria
+     *
+     * @param \Illuminate\Http\Request $request HTTP request
+     * 
+     * @author Okiemute Omuta <omuta.okiemute@gmail.com>
+     * 
+     * @return json
+     */
+    public function index(Request $request)
+    {
+        $intersted_columns = [
+            'name', 'isbn', 'authors', 'numberOfPages',
+            'publisher', 'country', 'released'
+        ];
+
+        try {
+            return $this->successRespons(collect(json_decode(
+                Http::get($this->api_url, [ 'name' => $request->query('name') ])))
+                    ->map(function ($item, $key) use ($intersted_columns) {
+                        return collect($item)->only($intersted_columns)
+                    ->keyBy(function($item, $key) {
+                        return Str::snake(str_replace('released', 'release_date', $key));
+                    });
+                })->toArray());
+        } catch (\Exception $exception) {
+            return $this->errorRespons($exception->getMessage(), 500);
+        }
+    }
+}
