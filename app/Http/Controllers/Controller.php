decdef805d7c94bf1e381545157d10e2ed2b9bbf
Okiemute Omuta <iamkheme@gmail.com>
2020-08-18 20:17:56 +0100

    Update Controller
    Updated methods to include message when present

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -27,37 +27,45 @@ class Controller extends BaseController
      *
      * @return json
      */
-    protected function successRespons(array $data = []) : \Illuminate\Http\JsonResponse
+    protected function successRespons(array $data = [], string $message = null) : \Illuminate\Http\JsonResponse
     {
-        return response()->json(
-            [
-                'status_code' => 200,
-                'status'      => 'success',
-                'data'        => $data,
-            ],
-            200
-        );
+        $response = [
+            'status_code' => 200,
+            'status'      => 'success',
+        ];
+
+        if ($message) {
+            $response['message'] = $message;
+        }
+
+        $response['data'] = $data;
+        
+        return response()->json($response, 200);
     }
 
     /**
      * Sends a success json response to the user
      *
-     * @param array $error_message (Optional) Message to include in the response. Defaults to null.
-     * @param array $status_code   (Optional) Status code for the response. Defaults to 400.
+     * @param array $message     (Optional) Message to include in the response. Defaults to null.
+     * @param array $status_code (Optional) Status code for the response. Defaults to 400.
      *
      * @author Okiemute Omuta <iamkheme@gmail.com>
      *
      * @return json
      */
-    protected function errorRespons(string $error_message = null, int $status_code = 400) : \Illuminate\Http\JsonResponse
+    protected function errorRespons(string $message = null, int $status_code = 400) : \Illuminate\Http\JsonResponse
     {
-        return response()->json(
-            [
-                'status_code' => $status_code,
-                'status'      => 'failure',
-                'data'        => [ 'message' => $error_message, ],
-            ],
-            $status_code
-        );
+        $response = [
+            'status_code' => $status_code,
+            'status'      => 'failure',
+        ];
+
+        if ($message) {
+            $response['message'] = $message;
+        }
+
+        $response['data'] = [];
+        
+        return response()->json($response, $status_code);
     }
 }
