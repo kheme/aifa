17064466b6639c20fa8373344192faecd1e12487
Okiemute Omuta <iamkheme@gmail.com>
2020-08-20 10:36:42 +0100

    Create remaining books CRUD endpoints

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 $router->get('external-books',      'ExternalBookController@index');
 
-$router->post('api/v1/books',       'BookController@store');
+$router->get('api/v1/books',                'BookController@index');
+$router->get('api/v1/books/{book_id}',      'BookController@show');
+$router->patch('api/v1/books/{book_id}',    'BookController@update');
+$router->post('api/v1/books',               'BookController@store');
+$router->delete('api/v1/books/{book_id}',   'BookController@destroy');
