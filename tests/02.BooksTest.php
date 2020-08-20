4d02afca7d88c5def9101ed15174c18f9104aa07
Okiemute Omuta <iamkheme@gmail.com>
2020-08-20 17:40:55 +0100

    Update BooksTest
    - Included factories
    - Created testCanUpdateBook()

--- tests/02.BooksTest.php
+++ tests/02.BooksTest.php
@@ -5,7 +5,8 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class BooksTest extends TestCase
 {
-    protected $book_data = [];
+    protected $fake_books;
+
 
     /**
      * Test setup
@@ -19,15 +20,7 @@ class BooksTest extends TestCase
         parent::setUp();
         $this->beginDatabaseTransaction();
 
-        $this->book_data = [
-            'name'            => 'My First Book',
-            'isbn'            => '123-3213243567',
-            'authors'         => [ 'John Doe' ],
-            'number_of_pages' => 350,
-            'publisher'       => 'Acme Books',
-            'country'         => 'United States',
-            'release_date'    => '2019-08-01',
-        ];
+        $this->fake_books = factory(App\Models\Book::class, 5)->make();
     }
     /**
      * Can we books successfully?
@@ -38,10 +31,11 @@ class BooksTest extends TestCase
      */
     public function testCanCreateBook()
     {
-        $response = $this->json('POST', 'api/v1/books', $this->book_data);
-        $response->assertResponseStatus(200);
+        $response = $this->json('POST', 'api/v1/books', $this->fake_books[0]->toArray());
+        $response->assertResponseStatus(201);
         $response->seeJson([ 'status' => 'success' ]);
-        $response->seeJson($this->book_data);
+        $response->seeJson($this->fake_books[0]->toArray());
+        $this->seeInDatabase('books', collect($this->fake_books[0]->toArray())->except('authors')->toArray());
     }
 
     /**
@@ -53,26 +47,36 @@ class BooksTest extends TestCase
      */
     public function testCanListBooks()
     {
-        $this->testCanCreateBook();
-        $first_book = $this->book_data;
-
-        $this->book_data = [
-            'name'            => 'A Clash of Kings',
-            'isbn'            => '978-0553108033',
-            'authors'         => [ 'George R. R. Martin' ],
-            'number_of_pages' => 768,
-            'publisher'       => 'Bantam Books',
-            'country'         => 'United States',
-            'release_date'    => '1999-02-02',
-        ];
-
-        $this->testCanCreateBook();
-        $second_book = $this->book_data;
+        $this->json('POST', 'api/v1/books', $this->fake_books[1]->toArray());
+        $this->json('POST', 'api/v1/books', $this->fake_books[2]->toArray());
 
         $response = $this->json('GET', 'api/v1/books');
         $response->assertResponseStatus(200);
         $response->seeJson([ 'status' => 'success' ]);
-        $response->seeJson($first_book);
-        $response->seeJson($second_book);
+        $response->seeJson($this->fake_books[1]->toArray());
+        $response->seeJson($this->fake_books[2]->toArray());
+    }
+    
+    /**
+     * Can we update books successfully?
+     *
+     * @author Okiemute Omuta <iamkheme@gmail.com>
+     * 
+     * @return void
+     */
+    public function testCanUpdateBook()
+    {
+        $post_response = $this->json('POST', 'api/v1/books', $this->fake_books[3]->toArray());
+        $post_response->seeJson([ 'status' => 'success' ]);
+
+        $get_response = $this->json('GET', 'api/v1/books');
+        $get_response->assertResponseStatus(200);
+        $get_response->seeJson($this->fake_books[3]->toArray());
+        
+        $new_book_id = json_decode($get_response->response->getContent())->data[0]->id;
+
+        $patch_response = $this->json('PATCH', 'api/v1/books/' . $new_book_id, $this->fake_books[4]->toArray());
+        $patch_response->assertResponseStatus(200);
+        $this->seeInDatabase('books', collect($this->fake_books[4]->toArray())->except('authors')->toArray());
     }
 }
