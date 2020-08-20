c5af04f6c60b35c5935a68617f75a8480b7be42f
Okiemute Omuta <iamkheme@gmail.com>
2020-08-20 17:45:30 +0100

    Code shuffle in routes file

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-$router->get('external-books',      'ExternalBookController@index');
+$router->get('external-books',       'ExternalBookController@index');
 
-$router->get('api/v1/books',                'BookController@index');
-$router->get('api/v1/books/{book_id}',      'BookController@show');
-$router->patch('api/v1/books/{book_id}',    'BookController@update');
-$router->post('api/v1/books',               'BookController@store');
-$router->delete('api/v1/books/{book_id}',   'BookController@destroy');
+$router->get('api/v1/books',         'BookController@index');
+$router->get('api/v1/books/{id}',    'BookController@show');
+$router->patch('api/v1/books/{id}',  'BookController@update');
+$router->post('api/v1/books',        'BookController@store');
+$router->delete('api/v1/books/{id}', 'BookController@destroy');
