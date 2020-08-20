47693a3b5287ea15258aeec1f351b8869be0be83
Okiemute Omuta <iamkheme@gmail.com>
2020-08-20 17:40:21 +0100

    Create BookFactory

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Book factory
+ *
+ * PHP version 7
+ *
+ * @category  Factory
+ * @package   Database\factories
+ * @author    Okiemute Omuta <omuta.okiemute@gmail.com>
+ * @copyright 2020 Okiemute Omuta. All rights reserved.
+ * @license   All rights reserved.
+ * @link      https://twitter.com/kheme
+ */
+use App\Models\Book;
+use Faker\Generator as Faker;
+
+$factory->define(
+    Book::class,
+    function (Faker $faker) {
+        return [
+            'name'            => $faker->sentence(mt_rand(3, 6)),
+            'isbn'            => $faker->ean13(),
+            'authors'         => [ $faker->firstname . ' ' . $faker->lastname ],
+            'number_of_pages' => mt_rand(10, 1000),
+            'publisher'       => $faker->company,
+            'country'         => $faker->country,
+            'release_date'    => $faker->date('Y-m-d')
+        ];
+    }
+);
