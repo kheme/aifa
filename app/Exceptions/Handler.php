4a9eeed4033cab2d4e2ada945d830598eaa3347b
Okiemute Omuta <iamkheme@gmail.com>
2020-08-20 17:40:41 +0100

    Update exception handler.php
    Reformatted response for different exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Support\Str;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -49,6 +50,41 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        $status_code   = 500;
+        $error_message = $exception->getMessage();
+
+        if (strpos($error_message, '(SQL') !== false) {
+            $error_message = trim(substr($error_message, 0, strpos($error_message, '(SQL')));
+        }
+        
+        $get_file = $exception->getFile();
+        $file     = substr($get_file, strpos($get_file, "app\\"));
+        $file     = substr($get_file, strrpos($get_file, "\\") + 1);
+        
+        $error_message .= " in " . $file;
+        $error_message .= ":" . $exception->getLine();
+        
+        $status_code = $exception->status ?? $status_code;
+        
+        if (method_exists($exception, 'errors')) {
+            foreach ($exception->errors() as $field => $message) {
+                $error_message = "$field: " . $message[0];
+                break;
+            }
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            $error_message = 'Record not found!';
+            $status_code   = 404;
+        }
+        
+        return response()->json(
+            [
+                'status_code' => $status_code,
+                'status'      => 'error',
+                'message'     => $error_message
+            ],
+            $status_code
+        );
     }
 }
