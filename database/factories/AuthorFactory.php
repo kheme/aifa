6df70c30ca86c20e07a463e2890315c8c60790e5
Okiemute Omuta <iamkheme@gmail.com>
2020-08-20 21:21:14 +0100

    Create new factories
    - Author
    - BookAuthor

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Author factory
+ *
+ * PHP version 7
+ *
+ * @category  Factory
+ * @package   Database\factories
+ * @author    Okiemute Omuta <omuta.okiemute@gmail.com>
+ * @copyright 2020 Okiemute Omuta. All rights reserved.
+ * @license   All rights reserved.
+ * @link      https://twitter.com/kheme
+ */
+use App\Models\Author;
+use Faker\Generator as Faker;
+
+$factory->define(
+    Author::class,
+    function (Faker $faker) {
+        return [
+             'name' => $faker->firstname . ' ' . $faker->lastname,
+        ];
+    }
+);
