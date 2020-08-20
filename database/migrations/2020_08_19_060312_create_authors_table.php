9a8e7751beae44a21ee830957fa635e500abfe33
Okiemute Omuta <iamkheme@gmail.com>
2020-08-20 09:37:54 +0100

    Create new migration file for:
    - authors table
    - books table
    - book_authors table
    - Add FK constraints to book_authors table

---
+++ database/migrations/2020_08_19_060312_create_authors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->engine    = 'InnoDB';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->unsignedBigInteger('id', true);
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
